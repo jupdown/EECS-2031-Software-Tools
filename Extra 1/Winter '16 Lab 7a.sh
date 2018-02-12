9186323de7e0a23a303094cf1890b13126cb8887
jupdown <jupdown@yorku.ca>
2018-02-12 13:36:45 -0500

    attempting to add a directory

---
+++ Extra 1/Winter '16 Lab 7a.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# EECS 2031Z - Lab 4
+# Filename: Winter '16 Lab 7a.sh
+# Author: Stegmaier, John
+# Email: jupdown@my.yorku.ca
+# Login ID: jupdown
+
+if [ $# -eq 2 ]
+then
+	echo "the sum of" $1 "and" $2 "is" $(expr $1 + $2)
+else
+	echo "Winter '16 Lab 7.sh: requires two parameters"
+fi
---
+++ Extra 1/Winter '16 Lab 7b.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# EECS 2031Z - Extra Lab 1
+# Filename: Winter '16 Lab 7b.sh
+# Author: Stegmaier, John
+# Email: jupdown@my.yorku.ca
+# Login ID: jupdown
+
+find -maxdepth 1 -type f -writable
